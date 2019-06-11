686afee4e98815a10e7fa06d6ab1b3035a77212f
Bernard D. White <danquahwhite@gmail.com>
2019-06-11 18:49:27 +0000

    implemented basic tweets listings using Tweet component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
+import Tweet from './Tweet';
 
 class Dashboard extends Component{
     render() {
-        console.log(this.props);
         return (
             <div>
                 <h3 className='center'>Your Timeline</h3>
                 <ul className='dashboard-list'>
-                    {this.props.tweetsIds.map((tweetId => (
-                        <li key={tweetId}>
+                    {this.props.tweetsIds.map((id => (
+                        <li key={id}>
                             <div>
-                                TWEET ID: {tweetId}
+                                <Tweet id={id}/>
                             </div>
                         </li>
                     )))}
