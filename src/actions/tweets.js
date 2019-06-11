f70acfabe096056196697279eaf70262c9e3c84d
Bernard D. White <danquahwhite@gmail.com>
2019-06-11 21:43:48 +0000

    implemented handleAddTweet async action creator

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,17 @@
-import {saveLikeToggle} from  '../utils/api'
+import {saveLikeToggle, saveTweet} from  '../utils/api'
+import {showLoading, hideLoading} from 'react-redux-loading';
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
 export const TOGGLE_TWEET = 'TOGGLE_TWEET';
+export const ADD_TWEET = 'ADD_TWEET';
 
+
+function addTweet(tweet) {
+    return {
+        type: ADD_TWEET,
+        tweet
+    }
+}
 export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
@@ -30,5 +39,23 @@ export function handleToggleTweet(info) {
                 dispatch(toggleTweet(info));
                 alert('There was an error liking the tweet. Try again')
             })
+    }
+}
+
+export function handleAddTweet(text, replyingTo) {
+    return function (dispatch, getState) {
+        const {authedUser} = getState();
+        dispatch(showLoading());
+
+        saveTweet({
+            text,
+            author: authedUser,
+            replyingTo
+        })
+            .then((tweet)=> dispatch(addTweet(tweet)))
+            .then(()=>dispatch(hideLoading())
+            .catch((err)=>{
+                alert('An error occurred adding tweet. Try again')
+            }));
     }
 }
