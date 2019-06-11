6f14a68a7212478b03285185b20c444425c99d52
Bernard D. White <danquahwhite@gmail.com>
2019-06-11 16:06:58 +0000

    implemented setAuthUser action creator

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
 import {getInitialData} from '../utils/api';
 import {receiveTweets} from './tweets';
 import {receiveUsers} from './users';
+import {setAuthUser} from './authedUser'
+
+const AUTH_ID = 'tylermcginnis';
 
 export function handleInitialData() {
     return (dispatch)=>{
         return getInitialData().then(({users, tweets}) => {
             dispatch(receiveUsers(users));
             dispatch(receiveTweets(tweets));
+            dispatch(setAuthUser(AUTH_ID));
         })
     }
 }
