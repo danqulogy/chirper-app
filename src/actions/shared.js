915307e6a60cda6ce9a0baddaaa2440b5b5c55d6
Bernard D. White <danquahwhite@gmail.com>
2019-06-11 19:42:35 +0000

    refactored initial load to use off-the-shelf action creators from react-redux-loading package

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import {getInitialData} from '../utils/api';
 import {receiveTweets} from './tweets';
 import {receiveUsers} from './users';
 import {setAuthUser} from './authedUser'
+import {showLoading, hideLoading} from 'react-redux-loading';
 
 const AUTH_ID = 'tylermcginnis';
 
 export function handleInitialData() {
     return (dispatch)=>{
+        dispatch(showLoading());
         return getInitialData().then(({users, tweets}) => {
             dispatch(receiveUsers(users));
             dispatch(receiveTweets(tweets));
             dispatch(setAuthUser(AUTH_ID));
+            dispatch(hideLoading());
         })
     }
 }
